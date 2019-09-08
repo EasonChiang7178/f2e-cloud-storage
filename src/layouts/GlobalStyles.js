0080246ab230142b2dd47b269ba81ed09ff0861a
Eason Chiang <ishiu.chiang@gmail.com>
2019-09-08 10:09:03 +0800

    initialize project with styled-component

---
+++ src/layouts/GlobalStyles.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap');
+  body {
+    font-family: 'Helvetica Neue', -apple-system, BlinkMacSystemFont, "Helvetica", 'Segoe UI', 'Open Sans', sans-serif;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-tap-highlight-color: transparent;
+  }
+`;
+
+class GlobalStyles extends React.PureComponent {
+  render = () => (
+    <>
+      <Normalize />
+      <Global />
+    </>
+  );
+}
+
+export default GlobalStyles;
